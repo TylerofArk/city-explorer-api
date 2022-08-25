4edc9e041f1fffbed257a6408acf8781f0806dee
Tyler Main <tyler.l.main@gmail.com>
2022-08-25 01:46:21 -0500

    added movie.js, weather.js, backend work

--- server.js
+++ server.js
@@ -9,7 +9,6 @@ console.log('My first server');
 const express = require('express');
 require('dotenv').config();
 const cors = require('cors');
-const data = require('./data/weather.json')
 const axios = require('axios');
 const { response } = require('express');
 
@@ -32,13 +31,14 @@ app.get('/weather', getWeather);
 
 async function getWeather(request, response) {
 		const searchQuery = request.query.searchQuery;
-		const weatherUrl = `http://api.weatherbit.io/v2.0/forecast/daily?units=I&lat=${this.state.cityLat}&lon=${this.state.cityLon}&key=${process.env.WEATHER_API_KEY}`;
+		const lat = request.query.lat;
+		const lon = request.query.lon;
+		const weatherUrl = `http://api.weatherbit.io/v2.0/forecast/daily?units=I&lat=${lat}&lon=${lon}&key=${process.env.WEATHER_API_KEY}`;
 		try {
 			const weatherResponse = await axios.get(weatherUrl);
 			console.log(weatherResponse);
-			let cityName = request.query.city;
-			let dataToGroom = data.find(city => city.city_name === cityName);
-			let dataToSend = dataToGroom.data.map(object => {
+			const dataToGroom = weatherResponse.data;
+			const dataToSend = dataToGroom.data.map(object => {
 				return new Forecast(object);
 				
 			});
@@ -52,12 +52,37 @@ async function getWeather(request, response) {
 
 class Forecast {
 	constructor(weatherObj) {
-		this.date = weatherObj.valid_date;
+		this.date = weatherObj.weather.valid_date;
 		this.description = weatherObj.weather.description;
 	}
 }
 
 // Movie Route
+
+app.get('/movies', getMovie);
+
+async function getMovie (request, response) {
+	const city = request.query.searchQuery;
+	console.log(searchQuery);
+	const movieUrl = `https://api.themoviedb.org/3/movie/550?api_key=${process.env.MOVIE_API_KEY}&query=${city}`;
+
+try {
+	const showMovie = await axios.get(url);
+
+	const dataToSend = showMovie.data.results.map(movieObj => new Showtimes(movieObj));
+	response.status(200).send(dataToSend);
+} catch (error){
+	response.status(500).send('error: Sorry, something went wrong.');
+}
+};
+
+class Showtimes {
+	constructor(movieObj) {
+		this.name = movieObj.title;
+		this.overview = movieObj.overview;
+	}
+}
+
 
 // Catch all - needs to be at the bottom
 app.get('*', (request, response) => {
