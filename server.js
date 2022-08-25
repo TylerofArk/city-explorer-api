cc254a11d38bba54a771507a28de3b2a832c2bf7
Tyler Main <tyler.l.main@gmail.com>
2022-08-24 19:00:10 -0500

    added weather route api

--- server.js
+++ server.js
@@ -5,19 +5,20 @@ console.log('My first server');
 // All info per Express documentation
 // servers we require not import
 
-//bring in express
+//REQUIRES
 const express = require('express');
+require('dotenv').config();
 const cors = require('cors');
 const data = require('./data/weather.json')
+const axios = require('axios');
+const { response } = require('express');
 
-require('dotenv').config();
-// once express is in we need to use it
-
+// Express Instance:
 const app = express();
-
+const PORT = process.env.PORT || 3002;
 app.use(cors());
 
-const PORT = process.env.PORT || 3002;
+// Endpoints
 
 // ROUTES
 
@@ -27,22 +28,27 @@ app.get('/', (request, response) => {
 })
 
 // Weather Route
-app.get('/weather', (request, response) => {
-	try {
-		console.log(request.query);
-		let cityName = request.query.city;
-		let dataToGroom = data.find(city => city.city_name === cityName);
-		let dataToSend = dataToGroom.data.map(object => {
-			return new Forecast(object);
-			
-		});
-		
-		response.status(200).send(dataToSend);
-		} catch (error) {
-			next(error);
-		}
-		
-});
+app.get('/weather', getWeather);
+
+async function getWeather(request, response) {
+		const searchQuery = request.query.searchQuery;
+		const weatherUrl = `http://api.weatherbit.io/v2.0/forecast/daily?units=I&lat=${this.state.cityLat}&lon=${this.state.cityLon}&key=${process.env.WEATHER_API_KEY}`;
+		try {
+			const weatherResponse = await axios.get(weatherUrl);
+			console.log(weatherResponse);
+			let cityName = request.query.city;
+			let dataToGroom = data.find(city => city.city_name === cityName);
+			let dataToSend = dataToGroom.data.map(object => {
+				return new Forecast(object);
+				
+			});
+			response.status(200).send(dataToSend);
+			} catch (error) {
+				console.log('error message is: ', err);
+				response.status(500).send('server error')
+				next(error);
+			}
+};
 
 class Forecast {
 	constructor(weatherObj) {
@@ -51,9 +57,11 @@ class Forecast {
 	}
 }
 
+// Movie Route
+
 // Catch all - needs to be at the bottom
 app.get('*', (request, response) => {
-	response.status(404).send('This route does not exist.')
+	response.status(404).send('The page you are looking for does not exist.')
 })
 
 app.use((error, request, response, next) => {
