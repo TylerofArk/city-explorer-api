2cb969cdd27d8fbe00254f78492a187323a9f3b6
Tyler Main <tyler.l.main@gmail.com>
2022-08-28 15:13:04 -0500

    implement movie cache

--- modules/movie.js
+++ modules/movie.js
@@ -1,18 +1,30 @@
 'use strict';
 
 const axios = require('axios');
+const cache = require('./cache');
 
 async function getMovie (request, response) {
 	const city = request.query.city;
+	const key = 'Showtimes' + city;
 	console.log(city);
 	console.log(process.env.MOVIE_API_KEY)
 	const movieUrl = `http://api.themoviedb.org/3/search/movie/?api_key=${process.env.MOVIE_API_KEY}&query=${city}`;
 
 try {
+	if(cache[key] && (Date.now() - cache[key].timeStamp < 50000)){
+		console.log('cache movie hit');
+		response.status(200).send(cache[key].data)}
+		else {
+			console.log('cache movie miss');
 	const showMovie = await axios.get(movieUrl);
-console.log(showMovie)
+	console.log(showMovie)
 	const dataToSend = showMovie.data.results.map(movieObj => new Showtimes(movieObj));
+	cache[key] = {
+		data:dataToSend,
+		timeStamp:Date.now(),
+	}
 	response.status(200).send(dataToSend);
+}
 } catch (error){
 	response.status(500).send(error.message);
 }
